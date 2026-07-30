e8edc2baa750839aed79dd008fc2540a8fd159fe
Tuxzilla <tuxzilla@tuxzilla.com>
2026-07-30 10:52:38 -0500

    Deduct user balance when purchasing items
    Signed-off-by: Tuxzilla <tuxzilla@tuxzilla.com>

--- src/shops/buy/items.rs
+++ src/shops/buy/items.rs
@@ -14,7 +14,7 @@ use crate::{errors::Error, shops::buy::db::db_get_item};
 pub async fn buy(
     ctx: poise::Context<'_, crate::Data, Error>,
     #[description = "name of the item to buy"] item: String,
-    #[description = "how many you want"] quantity: i64, // ive always wondered if this should be an i64 or not.. shouldnt think about it.
+    #[description = "how many you want"] quantity: i64,
 ) -> Result<(), Error> {
     let uid = ctx.author().id.get() as i64;
     let cid = ctx.channel_id().get() as i64;
@@ -35,13 +35,18 @@ pub async fn buy(
     let item_id = found_item.id;
     let acquired_price = found_item.price * quantity;
 
+    let user_id = ctx.author().id.get() as i64;
+    let amount = -acquired_price;
+    let database = &ctx.data().database;
+
+    crate::users::helpers::edit_balance(user_id, amount, database).await?;
+
     db_add_inv_item(uid, item_id, quantity, acquired_price, &ctx.data().database).await?;
 
     // couldnt think of a name, sorry
     // maybe ill make this standard across the codebase...
     // if i do this more than once LOL
-    let perchance =
-        db_remove_item(cid, &found_item.name, quantity, &ctx.data().database).await?;
+    let perchance = db_remove_item(cid, &found_item.name, quantity, &ctx.data().database).await?;
 
     if !perchance {
         ctx.say(format!("Not enough stock of `{}`.", found_item.name))
